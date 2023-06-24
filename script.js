c0934740bad56088372a8458493be3e6d5d4b89b
Austin R <samichpower@yahoo.com>
2023-06-24 05:54:55 -0500

    Add basic system to add elements with button

---
+++ script.js
@@ -0,0 +1,11 @@
+//On clicking the new item button, a prompt will appear asking what to add. Whatever the user inputs will appear as a list-item. Beside each list item is a checkbox and an X. When the X is clicked the item is deleted.
+
+const addNewButton = document.querySelector('.add-new-li');
+const todoList = document.querySelector('.todo-list')
+
+addNewButton.addEventListener('click', () => {
+  let itemContent = prompt("What do you want to add?");
+  const newListItem = document.createElement('li');
+  newListItem.textContent = itemContent;
+  todoList.appendChild(newListItem);
+})
