34ffe4ff1f8e09b91ed945c8134e380e078d3276
Austin R <samichpower@yahoo.com>
2023-06-24 10:42:52 -0500

    Rename new li class

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 //On clicking the new item button, a prompt will appear asking what to add. Whatever the user inputs will appear as a list-item. Beside each list item is a checkbox and an X. When the X is clicked the item is deleted.
 
-const addNewItem = document.querySelector('.add-new-li');
+//PLAN: Finish the UI, then create a seperate branch to make the code for it.
+
+const addNewItem = document.querySelector('.new-li-button');
 const todoList = document.querySelector('.todo-list')
 
 addNewItem.addEventListener('click', () => {
