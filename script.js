09908e5ab14404e748d6c380a8254dd955046b31
Austin R <samichpower@yahoo.com>
2023-06-24 07:37:40 -0500

    Add delete button

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 //On clicking the new item button, a prompt will appear asking what to add. Whatever the user inputs will appear as a list-item. Beside each list item is a checkbox and an X. When the X is clicked the item is deleted.
 
-const addNewButton = document.querySelector('.add-new-li');
+const addNewItem = document.querySelector('.add-new-li');
 const todoList = document.querySelector('.todo-list')
 
-addNewButton.addEventListener('click', () => {
+addNewItem.addEventListener('click', () => {
   let itemContent = prompt("What do you want to add?");
-
   const newListItem = document.createElement('li');
   const newCheckbox = document.createElement('input');
+  const deleteButton = document.createElement('button');
+
   newCheckbox.setAttribute('type', 'checkbox');
   newCheckbox.className = 'checkbox';
+  deleteButton.className = 'delete-button';
 
   newListItem.textContent = itemContent;
+  deleteButton.textContent = 'X';
 
   newListItem.prepend(newCheckbox);
+  newListItem.prepend(deleteButton);
   todoList.appendChild(newListItem);
 })
