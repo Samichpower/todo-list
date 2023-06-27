68eeb241814f9fc24571d28492e1fab5c15117d8
Austin R <samichpower@yahoo.com>
2023-06-27 16:59:31 -0500

    Update class to id for new-todo button

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 //PLAN: Finish the UI, then create a seperate branch to make the code for it.
 
-const addNewItem = document.querySelector('.new-li-button');
+const addNewItem = document.querySelector('#new-li-button');
 const todoList = document.querySelector('.todo-list')
 
 addNewItem.addEventListener('click', () => {
